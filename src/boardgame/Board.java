dcdedf2749076d663dea587151980133b9a3658f
Lorenzo Mokwa <lorenzomokwa@outlook.com>
2023-10-22 00:31:23 -0400

    added error handling for piece position

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -7,7 +7,9 @@ public class Board {
   private Piece[][] pieces;
 
   public Board(int rows, int columns) {
-
+    if (rows < 1 || columns < 1) {
+      throw new BoardException("Error creating board: board must be at least 1 unit tall and 1 unit wide.");
+    }
     this.rows = rows;
     this.columns = columns;
 
@@ -35,12 +37,35 @@ public class Board {
   }
 
   public Piece piece(Position position) {
+      if (!positionExists(position)) {
+      throw new BoardException("Not a valid position.");
+    }
+
     return pieces[position.getRow()][position.getColumn()];
   }
 
   public void PlacePiece(Piece piece, Position position) {
+    if (isOccupied(position)) {
+      throw new BoardException("Position " + position + " already occupied.");
+    }
+
     pieces[position.getRow()][position.getColumn()] = piece;
     piece.position = position;
   }
 
+  private boolean positionExists(int row, int column) {
+    return row >= 0 && row < rows && column >= 0 && column > columns;
+  }
+
+  public boolean positionExists(Position position) {
+    return positionExists(position.getRow(), position.getColumn());
+  }
+
+  public boolean isOccupied(Position position) {
+    if (!positionExists(position)) {
+      throw new BoardException("Not a valid position.");
+    }
+  
+    return piece(position) != null;
+  }
 }
