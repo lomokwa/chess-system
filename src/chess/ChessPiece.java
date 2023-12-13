204f2e0bbfb7e67ddbaeb2bc1ffd9937a7406791
Lorenzo Mokwa <lorenzomokwa@outlook.com>
2023-12-13 14:53:24 -0300

    fixed rook not being able to capture pieces

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -17,7 +17,7 @@ public abstract class ChessPiece extends Piece {
     return color;
   }
 
-  protected Boolean isThereOpponentPiece(Position position) {
+  protected boolean isThereOpponentPiece(Position position) {
     ChessPiece p = (ChessPiece)getBoard().piece(position);
     return p != null && p.getColor() != color;
   }
