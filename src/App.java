7f5cadb913a16cb0c709c72b618343a16bbdb0d7
Lorenzo Mokwa <lorenzomokwa@outlook.com>
2023-12-17 22:11:52 -0300

    removed pieces from initial setup

--- src/App.java
+++ src/App.java
@@ -7,37 +7,37 @@ import chess.ChessPiece;
 import chess.ChessPosition;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws Exception {
+    Scanner input = new Scanner(System.in);
+    ChessMatch chessMatch = new ChessMatch();
+
+    while (true) {
+      try {
+        UI.clearScreen();
+        UI.printBoard(chessMatch.getPieces());
+        System.out.println();
+        System.out.print("Source: ");
+        ChessPosition source = UI.readChessPosition(input);
+
+        boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+        UI.clearScreen();
+        UI.printBoard(chessMatch.getPieces(), possibleMoves);
+
+        System.out.println();
+        System.out.print("Target: ");
+        ChessPosition target = UI.readChessPosition(input);
+
+        ChessPiece capturedPiece = chessMatch.moveChessPiece(source, target);    
+
+      } catch (ChessException e) {
+        System.out.println(e.getMessage());
+        input.nextLine();
+
+      } catch (InputMismatchException e) {
+        System.out.println(e.getMessage());
+        input.nextLine();
         
-        Scanner input = new Scanner(System.in);
-        ChessMatch chessMatch = new ChessMatch();
-
-        while (true) {
-            try {
-                UI.clearScreen();
-                UI.printBoard(chessMatch.getPieces());
-                System.out.println();
-                System.out.print("Source: ");
-                ChessPosition source = UI.readChessPosition(input);
-
-                boolean[][] possibleMoves = chessMatch.possibleMoves(source);
-                UI.clearScreen();
-                UI.printBoard(chessMatch.getPieces(), possibleMoves);
-
-                System.out.println();
-                System.out.print("Target: ");
-                ChessPosition target = UI.readChessPosition(input);
-
-                ChessPiece capturedPiece = chessMatch.moveChessPiece(source, target);    
-            } catch (ChessException e) {
-                System.out.println(e.getMessage());
-                input.nextLine();
-            } catch (InputMismatchException e) {
-                System.out.println(e.getMessage());
-                input.nextLine();
-            }
-            
-        }
-        
+      }
     }
+  }
 }
