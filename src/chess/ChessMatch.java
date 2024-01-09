4eb0311ca903197c7b155ba79e082170849e6233
Lorenzo Mokwa <lorenzomokwa@outlook.com>
2024-01-09 01:25:31 -0300

    added logic to keep track of available and captured pieces

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
@@ -11,6 +14,8 @@ public class ChessMatch {
   private int turn;
   private Color currentPlayer;
   private Board board;
+  private List<Piece> availablePieces = new ArrayList<>();
+  private List<Piece> capturedPieces = new ArrayList<>();
   
   public ChessMatch() {
     board = new Board(8, 8);
@@ -64,6 +69,11 @@ public class ChessMatch {
 
     board.placePiece(p, target);
 
+    if (capturedPiece != null) {
+      availablePieces.remove(capturedPiece);
+      capturedPieces.add(capturedPiece);
+    }
+
     return capturedPiece;
   }
 
@@ -94,6 +104,7 @@ public class ChessMatch {
 
   private void placeNewPiece(char column, int row, ChessPiece piece) {
     board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    availablePieces.add(piece);
   }
 
   public void InitialSetup() {
