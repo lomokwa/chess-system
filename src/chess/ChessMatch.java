2ce6ab8ac88cb94e557f1966fa11498389c591a7
Lorenzo Mokwa <lorenzomokwa@outlook.com>
2024-01-11 16:06:09 -0300

    added checkmate logic

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -16,6 +16,7 @@ public class ChessMatch {
   private Color currentPlayer;
   private Board board;
   private boolean check;
+  private boolean checkMate;
   private List<Piece> availablePieces = new ArrayList<>();
   private List<Piece> capturedPieces = new ArrayList<>();
   
@@ -36,6 +37,10 @@ public class ChessMatch {
 
   public boolean getCheck() {
     return check;
+  }
+
+  public boolean getCheckMate() {
+    return checkMate;
   }
 
   public ChessPiece[][] getPieces() {
@@ -65,7 +70,12 @@ public class ChessMatch {
 
     check = (testCheck(opponent(currentPlayer))) ? true : false;
 
-    nextTurn();
+    if (testCheckMate(opponent(currentPlayer))) {
+      checkMate = true;
+    } else {
+      nextTurn();
+    }
+
     return (ChessPiece)capturedPiece;
   }
 
@@ -155,6 +165,35 @@ public class ChessMatch {
     }
     return false;
   }
+
+  private boolean testCheckMate(Color color) {
+    if (!testCheck(color)) {
+      return false;
+    }
+
+    List<Piece> list = availablePieces.stream().filter(x -> ((ChessPiece)x).getColor() == color).collect(Collectors.toList());
+    for (Piece piece : list) {
+      boolean[][] mat = piece.possibleMoves();
+      for (int i = 0; i < board.getRows(); i++) {
+        for (int j = 0; j < board.getColumns(); j++) {
+          if (mat[i][j]) {
+            Position source = ((ChessPiece)piece).getChessPosition().toPosition();
+            Position target = new Position(i, j);
+
+            Piece capturedPiece = makeMove(source, target);
+            boolean testCheck = testCheck(color);
+
+            undoMove(source, target, capturedPiece);
+
+            if (!testCheck) {
+              return false;
+            }
+          }
+        }
+      }
+    }
+    return true;
+  }
   
 
   private void placeNewPiece(char column, int row, ChessPiece piece) {
@@ -168,6 +207,6 @@ public class ChessMatch {
     placeNewPiece('e', 1, new King(board, Color.WHITE));
 
     placeNewPiece('b', 8, new Rook(board, Color.BLACK));
-    placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+    placeNewPiece('a', 8, new King(board, Color.BLACK));
   }
 }
